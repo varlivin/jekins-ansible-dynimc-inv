02b3e29eef596cdc48de328ef77de86b0eceb05b
varlivin <163875305+varlivin@users.noreply.github.com>
2024-10-04 20:38:24 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
 provider "aws"{
-region = "ap-southeast-1"
+region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSInstance"{
-  ami = "ami-0d07675d294f17973"
-  instance_type = "t2.micro"
-  key_name = "devopsmssjune2024"
-  security_groups = ["launch-wizard-2"]
-  tags={
-  Name = "terraformserver"
-  }
+resource "aws_instance" "AWS_EC2INSTANCE"{
+ami = "ami-0fff1b9a61dec8a5f"
+instance_type = "t2.micro"
+key_name = "livindevops"
+security_groups = ["launch-wizard-2"]
+tags = {
+Name = "terraform"
+}
 }
