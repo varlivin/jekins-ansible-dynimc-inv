27a15a3150d2ea2c52392b8a4e3839ffe6e87514
varlivin <163875305+varlivin@users.noreply.github.com>
2024-10-04 20:58:25 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,9 +1,9 @@
 provider "aws"{
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "AWS_EC2INSTANCE"{
-ami = "ami-0fff1b9a61dec8a5f"
+ami = "ami-0fff1b9a61dec8a5f" 
 instance_type = "t2.micro"
 key_name = "livindevops"
 security_groups = ["launch-wizard-2"]
